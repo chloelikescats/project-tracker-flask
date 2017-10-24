e82fdcbf3d35738824273ecf0f2e9e3f61222d96
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-24 15:16:42 -0700

    Made a web app and templates with Flask

---
+++ hackbright_web.py
@@ -0,0 +1,59 @@
+"""A web application for tracking projects, students, and student grades."""
+
+from flask import Flask, request, render_template
+
+import hackbright
+
+app = Flask(__name__)
+
+
+@app.route("/student")
+def get_student():
+    """Show information about a student."""
+
+    github = request.args.get('github')
+
+    first, last, github = hackbright.get_student_by_github(github)
+
+    project_details = hackbright.get_grades_by_github(github)
+
+    html = render_template("student_info.html",
+                           first=first,
+                           last=last,
+                           github=github,
+                           project_details=project_details)
+    return html
+
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+
+@app.route("/add-student")
+def add_student():
+    """Add a student."""
+
+    html = render_template("add_student.html")
+
+    return html
+
+
+@app.route("/display-student", methods=['POST'])
+def display_student():
+    """Add new student to database."""
+
+    first_name = request.form.get('first')
+    last_name = request.form.get('last')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(first_name, last_name, github)
+
+    return render_template("new_student.html", github=github)
+
+
+if __name__ == "__main__":
+    hackbright.connect_to_db(app)
+    app.run(debug=True)
